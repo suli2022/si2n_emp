0ee4f551c8fc6319b0c40c3c69215fb2c9a329f7
Nagy Janos <nagy@zold.lan>
2023-03-10 10:45:30 +0100

    A dolgozók tömbje és a táblázat kész

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const tbody = document.querySelector('#tbody');
+var dolgozoLista = [
+    { name: 'Pisti', city: 'Szolnok', salary: 385 },
+    { name: 'Mari', city: 'Szeged', salary: 382 },
+    { name: 'Kati', city: 'Szolnok', salary: 355 },
+    { name: 'Dani', city: 'Szeged', salary: 384 },
+    { name: 'Lili', city: 'Szolnok', salary: 382 }
+];
+
+console.log(tbody)
